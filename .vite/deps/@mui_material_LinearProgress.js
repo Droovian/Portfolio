0b9d29a8948538327f94b31169625c9eec01a331
Dhruv Naik <dhruviscracked@gmail.com>
2023-11-27 22:35:20 +0530

    Images fixed

---
+++ .vite/deps/@mui_material_LinearProgress.js
@@ -0,0 +1,14 @@
+"use client";
+import {
+  LinearProgress_default,
+  getLinearProgressUtilityClass,
+  linearProgressClasses_default
+} from "./chunk-ATHQPBJE.js";
+import "./chunk-AY3GZSZL.js";
+import "./chunk-BHZAVQOK.js";
+export {
+  LinearProgress_default as default,
+  getLinearProgressUtilityClass,
+  linearProgressClasses_default as linearProgressClasses
+};
+//# sourceMappingURL=@mui_material_LinearProgress.js.map
--- data/projects.jsx
+++ data/projects.jsx
@@ -1,47 +1,41 @@
+import React from 'react';
 import Work from "../src/components/Work";
+import mapImage from '../src/assets/map.png';
+import indexImage from '../src/assets/index.png';
+import swiftForecastImage from '../src/assets/swiftforecast.png';
+import bloggerImage from '../src/assets/blogger.png';
+
 const projects = () => {
 
     const proj = [
         {
             id: 1,
             title: "Property Rental",
-            description: "A PHP, MySQL site with AUTH, Listings, Bookings, Payment & Customer Reviews.(Not yet public)",
-            images: [
-                "../src/assets/map.png",
-                "../src/assets/index.png",
-                "../src/assets/map.png"
-            ],
+            description: "A PHP, MySQL site with AUTH, Listings, Bookings, Payment & Customer Reviews. (Not yet public)",
+            images: [mapImage, indexImage, mapImage],
             gitLink: 'https://github.com/Droovian/Renter',
         },
         {
             id: 2,
             title: "Swift-Forecast",
-            description: "Real Time Weather site built with Axios, TailwindCSS. Learnt state with this Project.",
-            images: [
-                "../src/assets/swiftforecast.png",
-                "url-to-image5",
-                "url-to-image6"
-            ],
+            description: "Real Time Weather site built with Axios, TailwindCSS. Learned state with this Project.",
+            images: [swiftForecastImage, /* Update with actual image paths */, /* Update with actual image paths */],
             gitLink: 'https://github.com/Droovian/Swift-Forecast',
-            
         },
         {
             id: 3,
             title: "Blogger",
             description: "Add, delete and modify blog data with this project I've named Blogger built with JSON-Server.",
-            images: [
-                "../src/assets/blogger.png",
-                "url-to-image5",
-                "url-to-image6"
-            ],
+            images: [bloggerImage, /* Update with actual image paths */, /* Update with actual image paths */],
             gitLink: 'https://github.com/Droovian/Blogger',
         }
     ];
+
     return ( 
         <div>
             <Work projects={proj}/>
         </div>
-     );
+    );
 }
- 
+
 export default projects;
