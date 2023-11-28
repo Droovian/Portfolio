75ed10df9d811776bd5773f5b9fb738a1325428a
Dhruv Naik <dhruviscracked@gmail.com>
2023-11-28 15:51:43 +0530

    form has better error handling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,83 +1,109 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import LinearProgress from "@mui/material/LinearProgress";
-
 import { Link } from "react-router-dom";
+
 const Contact = () => {
+  const [isSubmitted, setisSubmitted] = useState(false);
+  const [timer, setTimer] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const [isSubmitted, setisSubmitted] = useState(false);
-    const [timer, setTimer] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const handleSubmit = async () => {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000); 
 
-    const formWait = () => {
-        setTimeout(() => {
-            setTimer(true);
-        }, 7000);
-    }
+    const startTime = new Date();
 
-    const handleSubmit = async () => {
+    setIsLoading(true);
 
-        formWait();
-        setIsLoading(true);
-    
-        try {
-            const response = await fetch('https://formsubmit.co/dde486e9072e97fb65b8dcd3ae9ab3f2', {
-                method: 'post',
-            });
-    
-            console.log('Response Status:', response.status);
-    
-            if (response.ok) {
-                console.log('Form submitted successfully!');
-                setisSubmitted(true);
-                setError(null);
-            } else {
-                const errorMessage = await response.text();
-                console.error(`Form submission failed. Status: ${response.status}, Message: ${errorMessage}`);
-                setisSubmitted(false);
-                setError('Form submission failed. Please try again.');
-            }
-        } catch (err) {
-            console.error(err);
-            setisSubmitted(false);
-            setError('Form submission failed. Please try again.');
-        } finally {
-            setIsLoading(false);
+    try {
+      const response = await fetch('https://formsubmit.co/dde486e9072e97fb65b8dcd3ae9ab3f2', {
+        method: 'post',
+        signal: controller.signal, 
+      });
+
+      console.log('Response Status:', response.status);
+
+      clearTimeout(timeoutId); // Clear the timeout if the request completes before the timeout
+
+      const endTime = new Date();
+      const elapsedTime = endTime - startTime;
+
+      if (response.ok) {
+        console.log('Form submitted successfully!');
+        setisSubmitted(true);
+        setError(null);
+      } else {
+        const errorMessage = await response.text();
+        console.error(`Form submission failed. Status: ${response.status}, Message: ${errorMessage}`);
+
+        if (elapsedTime > 5000) {
+          setTimer(true);
         }
-    };
-    
-    return ( 
+
+        setisSubmitted(false);
+        setError('Form submission failed. Please try again.');
+      }
+    } catch (err) {
+      if (err.name === 'AbortError') {
+        console.error('Form submission timed out. Please try again.');
+        setTimer(true);
+      } else {
+        console.error(err);
+        setisSubmitted(false);
+        setError('Form submission failed. Please try again.');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
     <div className="bg-blue-950 w-screen h-screen">
-        <div className="absolute top-0 left-0 m-3">
-            <button className="px-3 py-2 border-b-2 border-blue-500 text-white">
-            <Link to='/'>Back To Home</Link>
-            </button>
-        </div>
-        <div className="flex h-screen">
+      <div className="absolute top-0 left-0 m-3">
+        <button className="px-3 py-2 border-b-2 border-blue-500 text-white">
+          <Link to="/">Back To Home</Link>
+        </button>
+      </div>
+      <div className="flex h-screen">
         <div className="mt-10 w-full flex flex-col justify-center items-center">
-                <h1 className="font-bold text-4xl border-b-4 border-blue-500 text-gray-300 mb-3">Contact</h1> 
-                { timer ? (
-                    <p className="text-blue-300 mx-auto">This is taking longer than expected...</p>
-                ) : null }
-                {isSubmitted ? (
-                        <p className="text-blue-300 mx-auto">Form submitted successfully!</p>
-                    ) : (  <p className="text-red-500 mx-auto">{error}</p> )}
-                <form action="https://formsubmit.co/dde486e9072e97fb65b8dcd3ae9ab3f2" method="post" className="text-black flex flex-col space-y-3 w-3/4 sm:w-1/2" enctype="multipart/form-data" onSubmit={handleSubmit}>
-                    <input type="text" name='name' className="rounded-md outline-none p-2 bg-gray-200" autoComplete="off" placeholder="Name" required/>
-                    <input type="email" name="email" className="rounded-md outline-none p-2 bg-gray-200 " autoComplete="off" placeholder="Email" required/>
-                    <textarea name="message" id="" cols="30" rows="15" placeholder="Message" className="shadow-lg rounded-md outline-none p-2 bg-gray-200" required></textarea>
-                    <button type="submit" className="bg-transparent w-32 mx-auto text-gray-200 px-4 py-1 border hover:bg-blue-500 hover:border-blue-500"
-                    disabled={isLoading}>Message</button>
-                    {isLoading ? <Box sx={{ width: '100%' }}>
-                    <LinearProgress />
-                    </Box> : null}
-                </form>
-                    
+          <h1 className="font-bold text-4xl border-b-4 border-blue-500 text-gray-300 mb-3">Contact</h1>
+          {timer ? (
+            <p className="text-blue-300 mx-auto">This is taking longer than expected...</p>
+          ) : null}
+          {isSubmitted ? (
+            <p className="text-blue-300 mx-auto">Form submitted successfully!</p>
+          ) : (
+            <p className="text-red-500 mx-auto">{error}</p>
+          )}
+          <form
+            action="https://formsubmit.co/dde486e9072e97fb65b8dcd3ae9ab3f2"
+            method="post"
+            className="text-black flex flex-col space-y-3 w-3/4 sm:w-1/2"
+            encType="multipart/form-data"
+            onSubmit={handleSubmit}
+          >
+            <input type="text" name="name" className="rounded-md outline-none p-2 bg-gray-200" autoComplete="off" placeholder="Name" required />
+            <input type="email" name="email" className="rounded-md outline-none p-2 bg-gray-200 " autoComplete="off" placeholder="Email" required />
+            <textarea name="message" id="" cols="30" rows="15" placeholder="Message" className="shadow-lg rounded-md outline-none p-2 bg-gray-200" required></textarea>
+            <button
+              type="submit"
+              className="bg-transparent w-32 mx-auto text-gray-200 px-4 py-1 border hover:bg-blue-500 hover:border-blue-500"
+              disabled={isLoading}
+            >
+              Message
+            </button>
+            {isLoading ? (
+              <Box sx={{ width: '100%' }}>
+                <LinearProgress />
+              </Box>
+            ) : null}
+          </form>
         </div>
-        </div>
-    </div> 
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
